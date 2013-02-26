19e3ef5050fe4fbca99024661979bc3bf10c3e4f
Mike Jacobi <mikerjacobi@gmail.com>
2013-02-25 21:51:43 -0800

    added prelim metadata

--- server.py
+++ server.py
@@ -34,6 +34,25 @@ class Template(object):
                 return output
         index.exposed=True
 
+class Session(object):
+	def index(self, sesh=None):
+		if sesh!=None: 
+			sesh=str(sesh)
+			cherrypy.session['sesh'] = sesh
+			print 'sesh',cherrypy.session.get('sesh')
+		output=''
+		pageurl=webpageDirectory+'session.html'
+        	f=open(pageurl,'r')
+		for l in f:
+                        if 'headergoeshere' in l: output+=header()
+                        elif 'footergoeshere' in l: output+=footer()
+			elif 'showsesh' in l:	
+				if sesh!=None: 
+					output+='your session variable is %s'%(cherrypy.session.get('sesh'))
+                        else: output+=l
+                return output
+        index.exposed=True
+
 class Load(object):
         def index(selfi, musiclist=None):
 		if musiclist!=None:
@@ -44,17 +63,36 @@ class Load(object):
                 for l in f:
                         if 'headergoeshere' in l: output+=header()
                         elif 'footergoeshere' in l: output+=footer()
+			elif 'loadlist' in l:
+				if musiclist==None: continue
+				playlist=list(c['lists'][musiclist].find())
+				for i in range(len(playlist)):
+					name=playlist[i]['song']
+					artist=playlist[i]['artist']
+					url=playlist[i]['url']
+					
+					output+="%d: <a href=%s>%s by %s</a>  <br>\n"%(i+1, url,name,artist)
+					#for metadata in song:
+						#print metadata
+						#output+=str(metadata)
                         else: output+=l
                 return output
         index.exposed=True
 		
 
 class Lists(object):
-	def index(self,name=None,url=None):
+	def index(self,listname=None,url=None,song=None,artist=None):
 		added=0
-		if name!=None and url!=None:
+		if listname!=None and url!=None and song!=None and artist!=None:
+			listname,url,song,artist=str(listname),str(url),str(song),str(artist)
+		
 			#logic to add to mongo
-			c['lists'][str(name)].insert({'url':str(url)})
+			url=url.split('=')[0].split('?')[0]
+			url="http://www.youtube.com/embed/%s"%(url)
+			#verify that it's a real url
+
+			
+			c['lists'][listname].insert({'url':url,'song':song,'artist':artist})
 			added=1
 
 		output=''
@@ -64,7 +102,7 @@ class Lists(object):
 			elif 'footergoeshere' in l: output+=footer()
 			elif "name=content" in l and added:
 				output+=l
-				output+='added '+str(url)+' to '+str(name)+'!<br>'	
+				output+='added '+song+' to '+listname+'!<br>'	
 			else: output+=l
 		return output
 	index.exposed=True
@@ -101,6 +139,11 @@ root=Index()
 root.convert=Convert()
 root.lists=Lists()
 root.load=Load()
+root.session=Session()
 
-cherrypy.config.update({'server.socket_host':'0.0.0.0','server.socket_port':8080,'tools.sessions.on':True})
+cherrypy.config.update({'server.socket_host':'0.0.0.0',
+			'server.socket_port':8080,
+			'tools.sessions.on':True,
+			
+			})
 cherrypy.quickstart(root)
